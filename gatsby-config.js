68c075ad6461fb46663b4f256a65885f2c4bb4a0
TomDevBr <tomasjocalheiros@gmail.com>
2025-02-06 17:56:12 -0300

    add config to algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,17 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
+require("dotenv").config()
 
-/**
- * @type {import('gatsby').GatsbyConfig}
- */
+const queries = require("./src/utils/algolia_queries")
+
 module.exports = {
   siteMetadata: {
-    title: `My blog`,
-    position: `Full stack Developer`,
-    description: `A blog about frontend development`,
+    title: `John Doe`,
+    position: `Backend Developer`,
+    description: `A blog about backend development and other cool stuff.`,
     author: `@myblog`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
     // needs to be the first to work with gatsby-remark-images
     {
       resolve: `gatsby-source-filesystem`,
@@ -60,10 +56,19 @@ module.exports = {
         ],
       },
     },
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000, // default: 1000
+        enablePartialUpdates: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -71,12 +76,13 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
